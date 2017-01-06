1ba1a4b8822df2b5fbbcfe2c27338989cf8e81df
sreejithtt <sreejithtt@gmail.com>
2017-01-06 09:51:18 +0530

    Add files via upload

---
+++ DemoIntegrationTest.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoIntegrationTest : MonoBehaviour {
+
+	//private float _elapsedTime = 0.0f;
+
+	private bool _isActive = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (_isActive) {
+			//_elapsedTime += Time.deltaTime;
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (_isActive) {
+			//Testing.
+		}
+	}
+}
